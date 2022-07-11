b92e0dbcffd072488167ad7094a8713ea26f4f39
Shanu Raj <shanuraj715@gmail.com>
2022-07-12 01:47:35 +0530

    added import and export line in utils/index

--- utilities/index.js
+++ utilities/index.js
@@ -1,5 +1,7 @@
 import { copyArray } from './copyArray/copyArray'
 import { random as randomNumber } from './randomNumber/randomNumber'
 import { wordsFirstCharUppercase } from './wordsFirstCharUppercase/wordsFirstCharUppercase'
+import {log, error, warn } from './log/log'
 
 export { copyArray, randomNumber, wordsFirstCharUppercase }
+export { log, warn, error }
